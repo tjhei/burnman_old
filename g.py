fd43befd7e051731a31cbe66c68f821409605ef0
Timo Heister <timo.heister@googlemail.com>
2012-07-31 11:46:30 -0500

    more tests

--- g.py
+++ g.py
@@ -17,10 +17,10 @@ import scipy.linalg
 
 
 #
-# inp = {'MgO':beta, 'FeO': 0, 'SiO2': gamma, 'CaO':0, 'Al2O3':0.1}
-# params = {'Fe in pv': 0.9, 'Ca in pv':0.1, 'Al in pv', 'Fe in fp':1.0}
+# inp = {'MgO':beta, 'FeO': , 'SiO2': gamma, 'CaO':, 'Al2O3':}
+# params = {'Fe in pv': , 'Ca in pv':, 'Al in pv', 'Fe in fp':}
 # ret = 'mol pv' A, 'mol fp' B, 'mol st' C
-#, 'Mg in pv':0, 'Fe in pv':0, 'Ca in pv':0,'Si in pv':0, 'Al in pv':0, 'Mg in fp':0,'Fe in fp':0,}
+# 'Mg in pv':0, 'Fe in pv':0, 'Ca in pv':0,'Si in pv':0, 'Al in pv':0, 'Mg in fp':0,'Fe in fp':0,}
 def determine_phases(inp, params):
 
     ret = {'mol pv':0, 'mol fp':0, 'mol st':0}
@@ -60,13 +60,44 @@ print determine_phases(inp, params)
 
 
 
-# test everything into pv
-inp = {'MgO':20, 'FeO': 0, 'SiO2':20, 'CaO':0, 'Al2O3':0.0}
-params = {'Fe in pv': 0.0, 'Ca in pv':0.0, 'Al in pv':0.0, 'Fe in fp':0.0}
-t = determine_phases(inp, params)
-assert t['mol pv'] == 20
-assert t['mol fp'] == 0
-assert t['mol st'] == 0
+def test_phases():
+    # test everything into pv
+    inp = {'MgO':20, 'FeO': 0, 'SiO2':20, 'CaO':0, 'Al2O3':0.0}
+    params = {'Fe in pv': 0.0, 'Ca in pv':0.0, 'Al in pv':0.0, 'Fe in fp':0.0}
+    t = determine_phases(inp, params)
+    assert t['mol pv'] == 20
+    assert t['mol fp'] == 0
+    assert t['mol st'] == 0
+
+    #
+    inp = {'MgO':10, 'FeO': 0, 'SiO2':0, 'CaO':0, 'Al2O3':0.0}
+    params = {'Fe in pv': 0.0, 'Ca in pv':0.0, 'Al in pv':0.0, 'Fe in fp':0.0}
+    t = determine_phases(inp, params)
+    assert t['mol pv'] == 10
+    assert t['mol fp'] == 0
+    assert t['mol st'] == 0
+
+    #
+    inp = {'MgO':10, 'FeO': 0, 'SiO2':3, 'CaO':0, 'Al2O3':0.0}
+    params = {'Fe in pv': 0.0, 'Ca in pv':0.0, 'Al in pv':0.0, 'Fe in fp':0.0}
+    t = determine_phases(inp, params)
+    assert t['mol pv'] == 7
+    assert t['mol fp'] == 3
+    assert t['mol st'] == 0
+
+    #
+    inp = {'MgO':3, 'FeO': 0, 'SiO2':7, 'CaO':0, 'Al2O3':0.0}
+    params = {'Fe in pv': 0.0, 'Ca in pv':0.0, 'Al in pv':0.0, 'Fe in fp':0.0}
+    t = determine_phases(inp, params)
+    assert t['mol pv'] == 3
+    assert t['mol fp'] == 0
+    assert t['mol st'] == 4
+
+
+
+
+test_phases()
+
 
 
 
