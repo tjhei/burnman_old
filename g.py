7d68cca98b3a14f5f744d0287b4b1bb20144684b
Timo Heister <timo.heister@googlemail.com>
2012-08-02 12:56:38 -0500

    pull out seismic computations

--- g.py
+++ g.py
@@ -1,11 +1,15 @@
+#system libs:
 import numpy
 import pylab
-import geotherm
+import scipy.optimize as opt
 import math
+
+#own libs:
+import geotherm
 import prem
 from tools import *
 from eos_from_ian import bm_density, birch_murnaghan
-import scipy.optimize as opt
+from seismic import *
 
 # TODO: add up weight percent and check <100 and tell them how much
 
@@ -129,79 +133,9 @@ def eqn_of_state(inp):
 
 
 
-# Reuss-Voigt-Hill average
-# inp: bulk modulus, shear modulus, density
-# return K_Si and gamma_i 
-def voigt_reuss_hill(molar_abundance, molar_weight, modulus, density, T):
-    # from matas 2007, Appendix C
-
-    n_phase = len(modulus)
-
-    assert n_phase == len(molar_abundance)
-    assert n_phase == len(molar_weight)
-    assert n_phase == len(density)
-
-    it = range(n_phase)
-
-    n_i = molar_abundance  # molar abundance for phase i 
-    M_i = molar_weight  # molar weight for phase i
-    total_molar = sum(n_i)
-    x_i = [(n_i[i] / total_molar) for i in it] # molar fraction of phase i
-    
-    #V_i = n_i * M_i / rho:
-    V_i = [(n_i[i]*M_i[i]/density[i]) for i in it]
-    #V = sum n_i V_i:
-    V = sum(i*j for i, j in zip(n_i,V_i))
-    #nu_i = x_i * V_i / V:
-    nu_i = [(x_i[i] * V_i[i] / V) for i in it]
-
-    #X_i = K_Si = K_Ti (1+alpha_i gamma_i T)
-    #alpha_i: thermal expansion
-    #gamma_i: grueneisen
-    #K_Ti: bulk modulus    
-    #not needed: X_i = [ (modulus[i] * (1.+thermal_exp[i] * grueneisen[i] * T)) for i in it]
-    X_i = modulus
-
-    #X_V = sum nu_i X_i:
-    X_V = sum(i*j for i,j in zip(nu_i,X_i))
-    #X_R = 1 / sum(nu_i/X_i):
-    X_R = 1. / sum(i/j for i,j in zip(nu_i,X_i))
-
-    return (X_V + X_R) / 2
-
-
-def calc_velocities(molar_abundance, molar_weight, bulk_modulus, shear_modulus, density, T):
-
-    it = range(len(molar_abundance))
-    n_i = molar_abundance  # molar abundance for phase i 
-    M_i = molar_weight  # molar weight for phase i
-    #V_i = n_i * M_i / density:
-    V_i = [(n_i[i]*M_i[i]/density[i]) for i in it]
-    #V = sum n_i V_i:
-    V = sum(i*j for i, j in zip(n_i,V_i))    
-    # avg_density = 1./ V sum(n_i M_i):
-    avg_density = 1./ V * sum((n_i[i]*M_i[i]) for i in it)
-
-    K_s = voigt_reuss_hill(molar_abundance, molar_weight, bulk_modulus, density, T)
-    mu = voigt_reuss_hill(molar_abundance, molar_weight, shear_modulus, density, T)
-    V_p = math.sqrt((K_s + 4./3. * mu) / avg_density )
-    V_s = math.sqrt(mu / avg_density)
-    V_phi = math.sqrt(K_s / avg_density)
-
-    return V_p,V_s,V_phi
 
 
 
-# test:
-
-molar_abundance=[1., 1., 1.]
-molar_weight=[1., 1., 1.]
-bulk_mod =[1., 1., 1.]
-shear_mod =[1., 1., 1.]
-density = [1., 1., 1.]
-T=1
-
-#print calc_velocities(molar_abundance, molar_weight, bulk_mod, shear_mod, density, T)
 
 
 #murakami test:
@@ -307,11 +241,12 @@ def murakami(molar_abundance):
         pv_shearmod.append(shear_mod[0])
         fp_shearmod.append(shear_mod[1])
 
-        result = calc_velocities(molar_abundance, molar_weight, bulk_mod, shear_mod, density, T)
+        result = calc_seismic_velocities(molar_abundance, molar_weight, bulk_mod, shear_mod, density, T)
         list_p.append(p)
         list_Vp.append(result[0])
         list_Vs.append(result[1])
 
+        # shearmodulus = density * V_s^2: (Sanne)
         prem_shearmod.append(prem.prem_density(p)*pow(prem.prem_V(p)[1],2.0))
 
     return list_p, list_Vs, list_Vp, pv_density, fp_density, pv_shearmod, fp_shearmod, prem_shearmod
